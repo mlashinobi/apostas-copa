4e720423bf655f669a47ddcdde6408e26210a4f6
mlashinobi <25danielribeiro01@gmail.com>
2026-06-15 18:08:43 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // V4 Compat: NÃO use export aqui.
 // Cole os dados do Firebase dentro deste objeto.
 window.firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI.firebaseapp.com",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI.appspot.com",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+  apiKey: "AIzaSyB0AoVeDWEiv0flCpOC-hvZYSUC0IH7WqE",
+  authDomain: "bolaocopa-9ea04.firebaseapp.com",
+  projectId: "bolaocopa-9ea04",
+  storageBucket: "bolaocopa-9ea04.firebasestorage.app",
+  messagingSenderId: "226209498109",
+  appId: "1:226209498109:web:97cab0d903f25dc1dacf94"
 };
